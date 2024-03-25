41e9a6936c24849215bd86c5657b63d1ae69e248
Seungji Baek <seungjibaek@Seungjis-MacBook-Pro.local>
2024-03-25 19:29:06 +0900

    feat: project 생성

---
+++ src/main/java/com/example/myrok/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.myrok.repository;
+
+import com.example.myrok.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
