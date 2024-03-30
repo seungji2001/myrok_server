d3c3a5de5b09c973a718abed1417b4004262c320
Seungji Baek <seungjibaek@Seungjis-MacBook-Pro.local>
2024-03-30 20:39:20 +0900

    chore: swagger붙이기

--- src/main/java/com/example/myrok/controller/ProjectController.java
+++ src/main/java/com/example/myrok/controller/ProjectController.java
@@ -3,6 +3,9 @@ package com.example.myrok.controller;
 import com.example.myrok.domain.Project;
 import com.example.myrok.dto.ProjectDto;
 import com.example.myrok.service.ProjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/project")
+@Tag(name = "Project", description = "Project 관련 API 입니다.")
 @RequiredArgsConstructor
 public class ProjectController {
 
     private final ProjectService projectService;
 
+    @Operation(
+            summary = "프로젝트 생성",
+            description = "프로젝트를 생성합니다"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "프로젝트 생성을 완료하였습니다."
+    )
     @PostMapping("/")
     public ResponseEntity<Long> createProject(Long memberId, @RequestBody ProjectDto projectDto){
         return ResponseEntity.ok().body(projectService.register(memberId, projectDto));
