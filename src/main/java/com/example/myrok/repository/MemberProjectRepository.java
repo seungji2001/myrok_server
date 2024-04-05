373935ec57ec5057d322b90f000cbbce03a9dfc5
Seungji Baek <seungjibaek@Seungjis-MacBook-Pro.local>
2024-04-05 17:22:54 +0900

    refact: member에 project 새로 등록

--- src/main/java/com/example/myrok/repository/MemberProjectRepository.java
+++ src/main/java/com/example/myrok/repository/MemberProjectRepository.java
@@ -1,9 +1,14 @@
 package com.example.myrok.repository;
 
 import com.example.myrok.domain.MemberProject;
+import jakarta.annotation.Nullable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberProjectRepository extends JpaRepository<MemberProject, Long> {
+    Optional<MemberProject> findByMemberId(Long memberId);
+    Optional<MemberProject> findByMemberIdAndProjectId(Long memberId, @Nullable Long projectId);
 }
---
+++ src/test/java/com/example/myrok/controller/ProjectControllerTests.java
@@ -0,0 +1,30 @@
+package com.example.myrok.controller;
+
+import com.example.myrok.dto.ProjectDto;
+import com.example.myrok.service.MemberService;
+import com.example.myrok.service.ProjectService;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+public class ProjectControllerTests {
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    ProjectService projectService;
+
+    @Test
+    public void createProject(){
+        memberService.checkMemberHaveProject(2L);
+        ProjectDto.RegisterProject registerProject = ProjectDto.RegisterProject.builder()
+                .projectName("test_member2")
+                .startDate("")
+                .endDate("")
+                .build();
+        Long projectId = projectService.register(registerProject);
+        log.info(memberService.registerProjectToMember(2L, projectId));
+    }
+}
