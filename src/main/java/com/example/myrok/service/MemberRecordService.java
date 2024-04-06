153bfe4e52ca4d74fa6a8ba7a8088d93b1258823
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-04-06 16:13:01 +0900

    #7 멤버 권한 부여

--- src/main/java/com/example/myrok/service/MemberRecordService.java
+++ src/main/java/com/example/myrok/service/MemberRecordService.java
@@ -2,10 +2,11 @@ package com.example.myrok.service;
 
 import com.example.myrok.domain.MemberRecord;
 import com.example.myrok.domain.Record;
+import com.example.myrok.type.Role;
 
 import java.util.List;
 
 public interface MemberRecordService {
-    void save(List<Long> members, Record record);
+    void save(List<Long> members, Record record, Role role);
     void delete(Long id);
 }
