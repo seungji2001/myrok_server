b02c85ee1356a70d5091850ba137278f315f879d
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-04-09 13:14:28 +0900

    feat: auth 구현

--- src/main/java/com/example/myrok/repository/MemberRepository.java
+++ src/main/java/com/example/myrok/repository/MemberRepository.java
@@ -3,7 +3,11 @@ package com.example.myrok.repository;
 import com.example.myrok.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.example.myrok.type.ELoginProvider;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findBySocialIdAndProvider(String socialId, ELoginProvider provider);
 }
