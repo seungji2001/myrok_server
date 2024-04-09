a471ca618ae054762b5e777fdb6c553d7c23a2e5
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-04-09 13:42:01 +0900

    feat: auth/google 구현

---
+++ src/main/java/com/example/myrok/dto/exception/ResponseDto.java
@@ -0,0 +1,61 @@
+package com.example.myrok.dto.exception;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.example.myrok.exception.CustomException;
+import com.example.myrok.type.ErrorCode;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+public record ResponseDto<T> (
+        @JsonIgnore HttpStatus httpStatus,
+        @NotNull Boolean success,
+        @Nullable T data,
+        @Nullable ExceptionDto error
+) {
+    public static <T> ResponseDto<T> ok(@Nullable final T data) {
+        return new ResponseDto<>(HttpStatus.OK, true, data, null);
+    }
+
+    public static <T> ResponseDto<T> created(@Nullable final T data) {
+        return new ResponseDto<>(HttpStatus.CREATED, true, data, null);
+    }
+
+    public static ResponseDto<Object> fail(final MissingServletRequestParameterException e) {
+        return new ResponseDto<>(
+                HttpStatus.BAD_REQUEST,
+                false,
+                null,
+                ExceptionDto.of(ErrorCode.MISSING_REQUEST_PARAMETER)
+        );
+    }
+
+    public static ResponseDto<Object> fail(final MethodArgumentNotValidException e) {
+        return new ResponseDto<>(
+                HttpStatus.BAD_REQUEST,
+                false,
+                null,
+                new ArgumentNotValidExceptionDto(e));
+    }
+
+    public static ResponseDto<Object> fail(final MethodArgumentTypeMismatchException e) {
+        return new ResponseDto<>(
+                HttpStatus.BAD_REQUEST,
+                false,
+                null,
+                ExceptionDto.of(ErrorCode.MISMATCH_PARAMETER_TYPE)
+        );
+    }
+
+    public static ResponseDto<Object> fail(final CustomException e) {
+        return new ResponseDto<>(
+                HttpStatus.BAD_REQUEST,
+                false,
+                null,
+                ExceptionDto.of(e.getErrorCode())
+        );
+    }
+}
