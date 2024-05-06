bd4117401ab2328b6c73c21266b1b41f6340ca7e
Seungji Baek <seungjibaek@Seungjis-MacBook-Pro.local>
2024-05-06 22:53:51 +0900

    error: security config 에러

--- src/main/java/com/example/myrok/config/SecurityConfig.java
+++ src/main/java/com/example/myrok/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.myrok.config;
+//package com.example.myrok.config;
 
 
 import lombok.RequiredArgsConstructor;
@@ -12,23 +12,23 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@RequiredArgsConstructor
-@Configuration(proxyBeanMethods = false)
-@ConditionalOnDefaultWebSecurity
-@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-public class SecurityConfig {
-
-    //private final CustomOAuth2UserService customOAuth2UserService;
-
-    @Bean
-    @Order(SecurityProperties.BASIC_AUTH_ORDER)
-    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .oauth2Login();
-
-        return http.build();
-    }
-}
+//
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@RequiredArgsConstructor
+//@Configuration(proxyBeanMethods = false)
+//@ConditionalOnDefaultWebSecurity
+//@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+//public class SecurityConfig {
+//
+//    //private final CustomOAuth2UserService customOAuth2UserService;
+//
+//    @Bean
+//    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+//    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .oauth2Login();
+//
+//        return http.build();
+//    }
+//}
