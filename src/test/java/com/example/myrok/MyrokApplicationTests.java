d78e1d36d96e968142cd4d4b224cc63a833bce73
seungji2001 <lisa2009343123456@gmail.com>
2024-06-14 01:53:55 +0900

    fix : 테스트 코드

---
+++ src/test/java/com/example/myrok/MyrokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myrok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
